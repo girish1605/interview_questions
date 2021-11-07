1bb0aa48581beb80c192f6672182287c6565837a
girish1605 <girishbhangale@outlook.com>
2021-11-07 21:24:37 +0530

    LG Soft interview question

---
+++ extract_bit_from_positon_to_position.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define CHAR_SIZE 8
+#define NIBBLE_SIZE 4
+
+int extract_bit_from_range(unsigned int from, unsigned int to, int num) {
+	return num << ((sizeof(num) * CHAR_SIZE) - to) \
+				>> ((sizeof(num) * CHAR_SIZE) - to) \
+				>> (from / NIBBLE_SIZE) * NIBBLE_SIZE;
+
+}
+
+int main() {
+	
+	int num = 0x44332211;
+
+	int output = extract_bit_from_range(1, 32, num);
+
+	printf("Output : %X\n", output);
+
+	return 0;
+}
